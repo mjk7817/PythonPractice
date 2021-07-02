006fa405e57dbbae2e60a864c9f54d27aa98e77e
mjk7817 <maigh.jammu@colorado.edu>
2021-07-02 15:23:22 -0600

    final rock paper scissors

--- Rock.py
+++ Rock.py
@@ -1,6 +1,40 @@
 import random
-choice = ["Rock", "Paper", "Scissors"]
+choice = ["Rock", "Paper", 'Scissors']
 computer = random.choices(choice)
 player = False
 cpuScore=0
 playerScore = 0
+
+while True:
+    player = input("Rock, Paper, or Scissors? ").capitalize()
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose! ", computer, "covers ", player )
+            cpuScore +=1
+        else:
+            print("You win!", player, " covers ", computer)
+            playerScore+=1
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose! ", computer, "cuts ", player)
+            cpuScore+=1
+        else:
+            print("You win!", player, " covers ", computer)
+            playerScore += 1
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose... ", computer, "smashes ", player)
+            cpuScore+=1
+        else:
+            print("You win! ", computer, "cuts ", player)
+            playerScore+=1
+    elif player == 'End':
+        print("Final Scores:")
+        print(f"CPU:{cpuScore}")
+        print(f"Player:{playerScore}")
+        break
+
+
+
